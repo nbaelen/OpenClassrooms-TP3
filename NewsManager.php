03aa8fc52cbd0ec3ab9b026814a6da41b967d3d4
nbaelen <n.baelen@gmail.com>
2018-05-11 03:22:34 +0200

    Définition des méthodes de NewsManager.php (et de leur PHPDOC)

--- NewsManager.php
+++ NewsManager.php
@@ -2,4 +2,52 @@
 
 abstract class NewsManager {
 
+    /**
+     * Méthode permettant l'ajout d'une News
+     * @param News $pNews | News à ajouter
+     * @return void
+     */
+    abstract protected function add(News $pNews);
+
+    /**
+     * Méthode permettant la suppression d'une News
+     * @param $pNewsName | l'ID ou le nom de la News à supprimer
+     * @return void
+     */
+    abstract protected function delete($pNewsName);
+
+    /**
+     * Méthode permettant l'édition d'une News
+     * @param News $pNews | News à éditer
+     * @return void
+     */
+    abstract protected function edit(News $pNews);
+
+    /**
+     * Méthode permettant la sauvegarde d'une News
+     * @param News $pNews | News à sauvegarder
+     * @return void
+     */
+    abstract public function save(News $pNews);
+
+    /**
+     * Méthode renvoyant le nombre de News sauvegardées
+     * @return int
+     */
+    abstract public function count();
+
+    /**
+     * Méthode permettant de récupérer une News à partir de son ID
+     * @param $pId | l'identifiant de la News à récupérer
+     * @return News
+     */
+    abstract public function get($pId);
+
+    /**
+     * Méthode permettant de récupérer une liste à partir de $pId renseigné, et d'une longueur $pNumber
+     * @param $pId | l'ID à partir duquel commence la liste
+     * @param $pNumber | la taille de la liste
+     * @return array
+     */
+    abstract public function getList($pId, $pNumber);
 }
